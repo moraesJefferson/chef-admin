73abf92864f0d1c1fca86455e46aa7dbe3e0093b
Jefferson Moraes <37252984+moraesJefferson@users.noreply.github.com>
2020-11-29 23:29:32 -0300

    correção lista de produtos

--- src/main/java/br/com/chefAdmin/bean/ItemPedidoBean.java
+++ src/main/java/br/com/chefAdmin/bean/ItemPedidoBean.java
@@ -34,6 +34,13 @@ public class ItemPedidoBean implements Serializable {
 		
 	private Produto produto;
 	
+	private List<Produto> listaProdutoPratos;
+	private List<Produto> listaProdutoEntradas;
+	private List<Produto> listaProdutoBebidas;
+	private List<Produto> listaProdutoSaladas;
+	private List<Produto> listaProdutoSobremesas;
+	
+	
 	@PostConstruct
 	public void init() {
 		try{		
@@ -47,12 +54,33 @@ public class ItemPedidoBean implements Serializable {
 	@Transacional
 	public void carregaListas() {
 		try {
-			this.listaProdutoCategoria = ArrayListMultimap.create();
+			//this.listaProdutoCategoria = ArrayListMultimap.create();
 			List<Produto> lista = new ArrayList<Produto>();
+			
+			this.listaProdutoPratos = new ArrayList<Produto>();
+			this.listaProdutoEntradas= new ArrayList<Produto>();
+			this.listaProdutoBebidas= new ArrayList<Produto>();
+			this.listaProdutoSaladas= new ArrayList<Produto>();
+			this.listaProdutoSobremesas= new ArrayList<Produto>();
+			
+			
+			
 			lista = produtoDao.findAll();
 			
 			for(Produto p  : lista) {
-				this.listaProdutoCategoria.put(p.getCategoria().getDescricao(), p);
+				//this.listaProdutoCategoria.put(p.getCategoria().getDescricao(), p);
+				
+				if(p.getCategoria().getDescricao().equals("Entradas")) {
+					this.listaProdutoEntradas.add(p);
+				}else if(p.getCategoria().getDescricao().equals("Saladas")) {
+					this.listaProdutoSaladas.add(p);
+				}else if(p.getCategoria().getDescricao().equals("Pratos")) {
+					this.listaProdutoPratos.add(p);
+				}else if(p.getCategoria().getDescricao().equals("Sobremesas")) {
+					this.listaProdutoSobremesas.add(p);
+				}else {
+					this.listaProdutoBebidas.add(p);
+				}
 			}
 			
 		}catch (Exception e) {
@@ -82,6 +110,46 @@ public class ItemPedidoBean implements Serializable {
 
 	public void setProduto(Produto produto) {
 		this.produto = produto;
+	}
+
+	public List<Produto> getListaProdutoPratos() {
+		return listaProdutoPratos;
+	}
+
+	public void setListaProdutoPratos(List<Produto> listaProdutoPratos) {
+		this.listaProdutoPratos = listaProdutoPratos;
+	}
+
+	public List<Produto> getListaProdutoEntradas() {
+		return listaProdutoEntradas;
+	}
+
+	public void setListaProdutoEntradas(List<Produto> listaProdutoEntradas) {
+		this.listaProdutoEntradas = listaProdutoEntradas;
+	}
+
+	public List<Produto> getListaProdutoBebidas() {
+		return listaProdutoBebidas;
+	}
+
+	public void setListaProdutoBebidas(List<Produto> listaProdutoBebidas) {
+		this.listaProdutoBebidas = listaProdutoBebidas;
+	}
+
+	public List<Produto> getListaProdutoSaladas() {
+		return listaProdutoSaladas;
+	}
+
+	public void setListaProdutoSaladas(List<Produto> listaProdutoSaladas) {
+		this.listaProdutoSaladas = listaProdutoSaladas;
+	}
+
+	public List<Produto> getListaProdutoSobremesas() {
+		return listaProdutoSobremesas;
+	}
+
+	public void setListaProdutoSobremesas(List<Produto> listaProdutoSobremesas) {
+		this.listaProdutoSobremesas = listaProdutoSobremesas;
 	}
 
 }
